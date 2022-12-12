bd6a8ef955ebb85a6f1091cab46ef16f9f61a47b
maxxborer <maxxborer@gmail.com>
2022-12-12 12:08:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require('express')
   , path = require('path')
   , bodyParser = require('body-parser')
   , favicon = require('serve-favicon')
-  , methodOverride = require('method-override')
-  , memjs = require('memjs');
+  , methodOverride = require('method-override');
+  // , memjs = require('memjs');
 
 require('dotenv').config()
 
@@ -18,18 +18,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+//   failover: true,  // default: false
+//   timeout: 1,      // default: 0.5 (seconds)
+//   keepAlive: true  // default: false
+// })
 
-// app.get('/', routes.index);
+app.get('/', function(req,res){
+  // mc.set('root', res, { expires: 600 });
+  res.sendFile(path.join(__dirname + '/index.html'));
+  // mc.get('root');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
-
-var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
-  failover: true,  // default: false
-  timeout: 1,      // default: 0.5 (seconds)
-  keepAlive: true  // default: false
-})
