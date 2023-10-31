282b64cdd9a04ab009a6c6d8b57bd8d936426fa2
Maxx Borrer <maxxborer@gmail.com>
2023-11-01 01:49:32 +0600

    update ui

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var express = require('express')
   , bodyParser = require('body-parser')
   , favicon = require('serve-favicon')
   , methodOverride = require('method-override');
-  // , memjs = require('memjs');
 
 require('dotenv').config()
 
@@ -13,21 +12,13 @@ var app = express();
 
 app.use(compression())
 app.set('port', process.env.PORT || 80);
-app.use(favicon(__dirname + '/public/favicon-196x196.png'));
+app.use(favicon(__dirname + '/public/android-chrome-192x192.png'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
-//   failover: true,  // default: false
-//   timeout: 1,      // default: 0.5 (seconds)
-//   keepAlive: true  // default: false
-// })
-
 app.get('/', function(req,res){
-  // mc.set('root', res, { expires: 600 });
   res.sendFile(path.join(__dirname + '/index.html'));
-  // mc.get('root');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
